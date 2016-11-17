f5a8e98e4ebc50aefc213efdf93a364c8e0c569b
hualun <hualun@mogujie.com>
2016-11-17 14:42:33 +0800

    add

---
+++ src/main/java/com/chlsmile/serializable/App.java
@@ -0,0 +1,13 @@
+package com.chlsmile.serializable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
